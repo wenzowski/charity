3544f518593b8f4adf42c967495f040f82d10ac4
Alexander Wenzowski <alexander@wenzowski.com>
2011-10-26 19:43:22 -0400

    add custom image support to pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -5,4 +5,12 @@ class Page < ActiveRecord::Base
   has_and_belongs_to_many :users
 
   before_validation :generate_slug
+
+  has_attached_file :image,
+    :storage => :s3,
+    :bucket => 'charity.heroku.com',
+    :s3_credentials => {
+      :access_key_id => ENV['S3_KEY'],
+      :secret_access_key => ENV['S3_SECRET']
+    }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111026212916) do
+ActiveRecord::Schema.define(:version => 20111026232816) do
 
   create_table "pages", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(:version => 20111026212916) do
     t.string   "google_checkout_merchant_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "pages_users", :id => false, :force => true do |t|
